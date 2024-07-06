0de81259d83243f65a1b01fcadfd875dfa7a8692
tushar7810 <141735609+tushar7810@users.noreply.github.com>
2024-07-06 10:40:22 +0530

    code added in hospital management

--- Hospital Management/patient.model.js
+++ Hospital Management/patient.model.js
@@ -1,7 +1,36 @@
 import mongoose from "mongoose";
 
 const patientSchema = new mongoose.Schema({
-
+    name : {
+        type: String,
+        required: true
+    },
+    diagnosedWith : {
+        type: String,
+        required : true
+    },
+    address : {
+        type: String,
+        required : true
+    },
+    age : {
+        type: Number,
+        required : true
+    },
+    bloodGroup : {
+        type: String,
+        enum : ["A+" , "AB" , "B+" , "O" , "A-" , "B-"],
+        required : true
+    },
+    gender : {
+        type: String,
+        enum : ["Male" , "Female", "Other"],
+        required : true
+    },
+    admittedIn : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Hospital'
+    }
 }, {timestamps : true})
 
 export const Patient = mongoose.model('Patient' ,patientSchema )
